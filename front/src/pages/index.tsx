a2354fd9469c06e3017b79f34cc0c5ceeb38749f
okakyo <0622okakyo@gmail.com>
2022-01-22 13:54:17 +0900

    新規作成ページへのリンクを追加

--- front/src/pages/index.tsx
+++ front/src/pages/index.tsx
@@ -1,7 +1,8 @@
 import type { NextPage } from 'next';
-import { Box } from '@chakra-ui/react';
+import { Box, Flex, Button } from '@chakra-ui/react';
 import { TodoCard } from '../components/general/cards/TodoCard';
 import useSWR from 'swr';
+import Link from 'next/link';
 
 const Home: NextPage = () => {
   const fetcher = (...args) => fetch(...args).then((res) => res.json());
@@ -10,6 +11,12 @@ const Home: NextPage = () => {
   if (!data) return <>Loading...</>;
   return (
     <Box py="3">
+      <Flex>
+        <Link href="/work/create" passHref>
+          <Button colorScheme="twitter">新規作成</Button>
+        </Link>
+        <Box></Box>
+      </Flex>
       {data.map((item: any) => {
         return <TodoCard key={item.title} id={item.id} title={item.title} />;
       })}
